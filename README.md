5cf5690ed05f06e289de0f12de52ca7f2ddb93db
fredfoda <fredhenriquediniz@gmail.com>
2023-06-02 01:58:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Tela Inicial
+![Inicial](https://user-images.githubusercontent.com/40778725/169345433-ea5db60e-df54-477a-a4d0-a97fdea54e7d.png)
+
+# Quiz
+Aplicação tem como base criar um jogo onde o usuário pode responder perguntas sobre diversas áreas.
+
+Antes de iniciar a clonagem do projeto é necessário ter instalado em sua máquina as seguintes ferramentas.
+
+NodeJS ou Yarn
+
+GitHub instalado em sua máquina
+
+Execute o seguinte comando no seu terminal git clone terminal: git remote add origin https://github.com/DanielSandrin/Quiz
+
+1º Baixar dependências do front-end: Com o projeto aberto no seu editor acesse o terminal do windows no diretório onde se contra o projeto front-end e execute os seguintes comandos: npm install ou yarn para baixar as dependências. Após baixar as dependencias pode se utilizar os seguintes comandos yarn start ou npm start para iniciar o projeto.
+
+🛠 Tecnologias
+As seguintes ferramentas foram usadas na construção do projeto:
+
+-ReactJS
+
+-css
---
+++ src/data/perguntas.js
@@ -0,0 +1,137 @@
+export const Perguntas =[
+  {
+    pergunta: 'Quais são os três predadores do reino animal reconhecidos pela habilidade de caçar em grupo, se camuflar para surpreender as presas e possuir sentidos apurados, respectivamente:',
+    opcoesResposta:[
+      {resposta: 'Orca, onça e tarântula', correta: false, alternativa: 'A)'},
+      {resposta: 'Tigre, gavião e orca', correta: false, alternativa: 'B)'},
+      {resposta: 'Leão, tubarão branco e urso cinzento', correta: false, alternativa: 'C)'},
+      {resposta: 'Hiena, urso branco e lobo cinzento', correta: true, alternativa: 'D)'},
+    ]
+  },
+  {
+    pergunta: 'Quanto tempo a luz do Sol demora para chegar à Terra?',
+    opcoesResposta:[
+      {resposta: ' 1 dia', correta: false, alternativa: 'A)'},
+      {resposta: '12 horas', correta: false, alternativa: 'B)'},
+      {resposta: '12 segundos', correta: false, alternativa: 'C)'},
+      {resposta: '8 minutos', correta: true, alternativa: 'D)'},
+    ]
+  },
+  {
+    pergunta: 'Quais as duas datas que são comemoradas em novembro?',
+    opcoesResposta:[
+      {resposta: 'Independência do Brasil e Dia da Bandeira', correta: false, alternativa: 'A)'},
+      {resposta: 'Black Friday e Dia da Árvore', correta: false, alternativa: 'B)'},
+      {resposta: 'Dia do Médico e Dia de São Lucas', correta: false, alternativa: 'C)'},
+      {resposta: 'Proclamação da República e Dia Nacional da Consciência Negra', correta: true, alternativa: 'D)'},
+    ]
+  },
+  {
+    pergunta: 'Qual o número mínimo de jogadores em cada time numa partida de futebol?',
+    opcoesResposta:[
+      {resposta: '9', correta: false, alternativa: 'A)'},
+      {resposta: '10', correta: false, alternativa: 'B)'},
+      {resposta: '8', correta: false, alternativa: 'C)'},
+      {resposta: '7', correta: true, alternativa: 'D)'},
+    ]
+  },
+  {
+    pergunta: 'Qual o número mínimo de jogadores em cada time numa partida de futebol?',
+    opcoesResposta:[
+      {resposta: 'Janeiro', correta: false, alternativa: 'A)'},
+      {resposta: 'Fevereiro', correta: false, alternativa: 'B)'},
+      {resposta: 'Junho', correta: false, alternativa: 'C)'},
+      {resposta: 'Maio', correta: true, alternativa: 'D)'},
+    ]
+  },
+  {
+    pergunta: 'O que a palavra legend significa em português?',
+    opcoesResposta:[
+      {resposta: 'Legenda', correta: false, alternativa: 'A)'},
+      {resposta: 'História', correta: false, alternativa: 'B)'},
+      {resposta: 'Legendário', correta: false, alternativa: 'C)'},
+      {resposta: 'Lenda', correta: true, alternativa: 'D)'},
+    ]
+  },
+  {
+    pergunta: 'Quais os países que têm a maior e a menor expectativa de vida do mundo?',
+    opcoesResposta:[
+      {resposta: 'Japão e Serra Leoa', correta: false, alternativa: 'A)'},
+      {resposta: 'Itália e Chade', correta: false, alternativa: 'B)'},
+      {resposta: 'Estados Unidos e Angola', correta: false, alternativa: 'C)'},
+      {resposta: 'Brasil e Congo', correta: true, alternativa: 'D)'},
+    ]
+  },
+  {
+    pergunta: 'Quantas casas decimais tem o número pi?',
+    opcoesResposta:[
+      {resposta: 'Centenas', correta: false, alternativa: 'A)'},
+      {resposta: 'Vinte', correta: false, alternativa: 'B)'},
+      {resposta: 'Milhares', correta: false, alternativa: 'C)'},
+      {resposta: 'Infinitas', correta: true, alternativa: 'D)'},
+    ]
+  },
+  {
+    pergunta: 'Qual o livro mais vendido no mundo a seguir à Bíblia?',
+    opcoesResposta:[
+      {resposta: 'O Senhor dos Anéis', correta: false, alternativa: 'A)'},
+      {resposta: 'Um Conto de Duas Cidades', correta: false, alternativa: 'B)'},
+      {resposta: 'O Pequeno Príncipe', correta: false, alternativa: 'C)'},
+      {resposta: 'Dom Quixote', correta: true, alternativa: 'D)'},
+    ]
+  },
+  {
+    pergunta: 'De onde é a invenção do chuveiro elétrico?',
+    opcoesResposta:[
+      {resposta: 'França', correta: false, alternativa: 'A)'},
+      {resposta: 'Inglaterra', correta: false, alternativa: 'B)'},
+      {resposta: 'Austrália', correta: false, alternativa: 'C)'},
+      {resposta: 'Brasil', correta: true, alternativa: 'D)'},
+    ]
+  },
+  {
+    pergunta: 'Qual motorista ganhou mais campeonatos de Fórmula 1?',
+    opcoesResposta:[
+      {resposta: 'Janeiro', correta: false, alternativa: 'A)'},
+      {resposta: 'Fevereiro', correta: false, alternativa: 'B)'},
+      {resposta: 'Junho', correta: false, alternativa: 'C)'},
+      {resposta: 'Michael Schumacher', correta: true, alternativa: 'D)'},
+    ]
+  },
+  {
+    pergunta: ' De qual cidade vieram os Beatles?',
+    opcoesResposta:[
+      {resposta: 'Janeiro', correta: false, alternativa: 'A)'},
+      {resposta: 'Fevereiro', correta: false, alternativa: 'B)'},
+      {resposta: 'Junho', correta: false, alternativa: 'C)'},
+      {resposta: 'Liverpool', correta: true, alternativa: 'D)'},
+    ]
+  },
+  {
+    pergunta: 'Qual dos impérios a seguir não possui um idioma escrito?',
+    opcoesResposta:[
+      {resposta: 'Romano', correta: false, alternativa: 'A)'},
+      {resposta: 'Egípcio', correta: false, alternativa: 'B)'},
+      {resposta: 'Azteca', correta: false, alternativa: 'C)'},
+      {resposta: 'Inca', correta: true, alternativa: 'D)'},
+    ]
+  },
+  {
+    pergunta: ' Quantos dias são necessários para a Terra orbitar o sol?',
+    opcoesResposta:[
+      {resposta: 'Janeiro', correta: false, alternativa: 'A)'},
+      {resposta: 'Fevereiro', correta: false, alternativa: 'B)'},
+      {resposta: 'Junho', correta: false, alternativa: 'C)'},
+      {resposta: '365', correta: true, alternativa: 'D)'},
+    ]
+  },
+  {
+    pergunta: 'Qual é o animal nacional da Austrália?',
+    opcoesResposta:[
+      {resposta: 'Janeiro', correta: false, alternativa: 'A)'},
+      {resposta: 'Fevereiro', correta: false, alternativa: 'B)'},
+      {resposta: 'Junho', correta: false, alternativa: 'C)'},
+      {resposta: 'Canguru vermelho', correta: true, alternativa: 'D)'},
+    ]
+  }
+]
